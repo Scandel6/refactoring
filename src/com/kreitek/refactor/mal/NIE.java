b4bb008e4723618ff7100489a34ce666dc0b2495
Salvador Candel <salvacandel@gmail.com>
2022-04-06 12:03:53 +0200

    Creating class NIE

---
+++ src/com/kreitek/refactor/mal/NIE.java
@@ -0,0 +1,11 @@
+package com.kreitek.refactor.mal;
+
+public class NIE implements DocumentoIdentidad{
+    private String numNie;
+
+    @Override
+    public boolean validar() {
+        return false;
+    }
+
+}
